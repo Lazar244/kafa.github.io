f295bd0c36e6babae3c007abb47e8736bad6cadf
Lazar244 <lazarmarjanovic24@gmail.com>
2022-11-17 11:28:16 +0100

    Novi

---
+++ Glavna.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Glavna.aspx.vb" Inherits="Pitanje.Glavna" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Da li zelis da idemo na kafu?</h2>
+       <img src="https://media.tenor.com/hI-oOVvwasYAAAAC/happy.gif" height="200"/>&nbsp;&nbsp;&nbsp;&nbsp;
+        <div>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Da :)" />
+        <p>
+            <asp:Button ID="Button2" runat="server" Text="Naravno da ne" />
+        </p>
+        <asp:Label ID="Label1" runat="server" Text="Izaberi dan:"></asp:Label>
+&nbsp;&nbsp;
+        <p>
+            <asp:RadioButton ID="RadioButton1" runat="server" Text="Ponedeljak" />
+            <asp:RadioButton ID="RadioButton5" runat="server" Text="Petak" />
+        </p>
+        <asp:RadioButton ID="RadioButton2" runat="server" Text="Utorak" />
+        <asp:RadioButton ID="RadioButton6" runat="server" Text="Subota:" />
+        <p>
+            <asp:RadioButton ID="RadioButton3" runat="server" Text="Sreda" />
+            <asp:RadioButton ID="RadioButton7" runat="server" Text="Nedelja" />
+        </p>
+        <p>
+            <asp:RadioButton ID="RadioButton4" runat="server" Text="Cetvrtak" />
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Izaberi grad:"></asp:Label>
+        </p>
+        <p>
+            <asp:RadioButton ID="RadioButton8" runat="server" Text="Svrljig" />
+        </p>
+        <p>
+            <asp:RadioButton ID="RadioButton9" runat="server" Text="Nis" />
+        </p>
+        <asp:Button ID="Button3" runat="server" Text="Potvrdi" />
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="&lt;3"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Glavna.aspx.vb
@@ -0,0 +1,67 @@
+﻿Public Class Glavna
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Visible = False
+        Label2.Visible = False
+        Label3.Visible = False
+        Button3.Visible = False
+        RadioButton1.Visible = False
+        RadioButton2.Visible = False
+        RadioButton3.Visible = False
+        RadioButton4.Visible = False
+        RadioButton5.Visible = False
+        RadioButton6.Visible = False
+        RadioButton7.Visible = False
+        RadioButton8.Visible = False
+        RadioButton9.Visible = False
+
+
+    End Sub
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Enabled = False
+        Button2.Text = "Nemoguce je izabrati ovu opciju"
+
+
+
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Visible = True
+        Label2.Visible = True
+        RadioButton1.Visible = True
+        RadioButton2.Visible = True
+        RadioButton3.Visible = True
+        RadioButton4.Visible = True
+        RadioButton5.Visible = True
+        RadioButton6.Visible = True
+        RadioButton7.Visible = True
+        RadioButton8.Visible = True
+        RadioButton9.Visible = True
+        Button3.Visible = True
+
+
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Visible = True
+        Label2.Visible = True
+        RadioButton1.Visible = True
+        RadioButton2.Visible = True
+        RadioButton3.Visible = True
+        RadioButton4.Visible = True
+        RadioButton5.Visible = True
+        RadioButton6.Visible = True
+        RadioButton7.Visible = True
+        RadioButton8.Visible = True
+        RadioButton9.Visible = True
+        Button3.Visible = True
+        Label3.Visible = True
+    End Sub
+
+    Protected Sub RadioButton9_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton9.CheckedChanged
+
+    End Sub
+End Class
